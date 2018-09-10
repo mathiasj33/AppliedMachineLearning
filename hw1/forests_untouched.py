5f008ff50ebe18b47ece35066bd31ad8c5cdc8ec
Mathias Jackermeier <mathias.jackermeier@tum.de>
2018-09-10 12:55:23 -0500

    Refactored forests_untouched.

--- hw1/forests_untouched.py
+++ hw1/forests_untouched.py
@@ -1,18 +1,12 @@
-import numpy as np
+from hw1.utils import *
 from sklearn.ensemble import RandomForestClassifier
 
-train = np.genfromtxt('data/train.csv', delimiter=',')
-train = train[1:, 1:]
-train_labels = train[:, 0]
-train = train[:, 1:]
+train, train_labels = load_train_data()
+val, val_labels = load_val_data()
 
-val = np.genfromtxt('data/val.csv', delimiter=',')
-val_labels = val[1:, 0]
-val = val[1:, 1:]
-
-for num_trees in [10,30]:
-    for depth in [4,16]:
+for num_trees in [10, 30]:
+    for depth in [4, 16]:
         clf = RandomForestClassifier(n_estimators=num_trees, max_depth=depth)
         clf.fit(train, train_labels)
-        print('Trees: {}, depth: {}, acc: {}'.format(num_trees, depth, clf.score(val, val_labels)))
-
+        print('Trees: {}, depth: {}, acc: {}'
+              .format(num_trees, depth, clf.score(val, val_labels)))
