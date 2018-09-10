02cc7ef29f68e30aa075bf8e7d337869639a02d7
Mathias Jackermeier <mathias.jackermeier@tum.de>
2018-09-10 12:51:21 -0500

    Refactored gaussian_stretched.

--- hw1/utils.py
+++ hw1/utils.py
@@ -35,13 +35,17 @@ def stretched_bbox(inst):
     inst[:400] = np.array(im).reshape(400)
 
 
-def save_mean_images(clf, folder, max_rows=None):
+def save_mean_images(clf, folder, stretched=False, max_rows=None):
     test = load_test_data(max_rows=max_rows)
+    if stretched:
+        np.apply_along_axis(stretched_bbox, axis=1, arr=test)
+        test = test[:, :400]
     preds = clf.predict(test)
     for i in range(10):
         images = test[(preds >= i - .1) & (preds <= i + .1)]  # account for floating point inaccuracy
         mean = np.mean(images, axis=0)
         to_binary(mean)
         # mean /= 255 -- to get 0s and 1s. However, PIL then misinterprets the data.
-        mean_img = Image.fromarray(mean.reshape(28, 28))
+        size = 20 if stretched else 28
+        mean_img = Image.fromarray(mean.reshape(size, size))
         mean_img.convert('L').save('{}/{}/{}.png'.format(MEAN_PATH, folder, i))
