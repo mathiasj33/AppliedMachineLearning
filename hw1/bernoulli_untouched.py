d0a1717b888947e4b101e0c2b76fc1834e9afcca
Mathias Jackermeier <mathias.jackermeier@tum.de>
2018-09-10 13:00:37 -0500

    Refactored bernoulli_untouched.

--- hw1/bernoulli_untouched.py
+++ hw1/bernoulli_untouched.py
@@ -1,23 +1,14 @@
-import numpy as np
+from hw1.utils import *
 from sklearn.naive_bayes import BernoulliNB
 
-
-def to_binary(img):
-    img[img > 127] = 255.0
-    img[img <= 127] = 0.0
-
-
-train = np.genfromtxt('data/train.csv', delimiter=',')
-train = train[1:, 1:]
-train_labels = train[:, 0]
-train = train[:, 1:]
+train, train_labels = load_train_data()
 to_binary(train)
 
-val = np.genfromtxt('data/val.csv', delimiter=',')
-val_labels = val[1:, 0]
-val = val[1:, 1:]
+val, val_labels = load_val_data()
 to_binary(val)
 
 clf = BernoulliNB()
 clf.fit(train, train_labels)
 print(clf.score(val, val_labels))
+
+save_mean_images(clf, 'bernoulli_untouched')
