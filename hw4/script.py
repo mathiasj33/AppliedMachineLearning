98ccfa3f8333b1f518b6b8e9442991beaa685fe0
Mathias Jackermeier <mathias.jackermeier@tum.de>
2018-09-26 23:39:59 -0500

    Implemented pca mse plot.

--- hw4/script.py
+++ hw4/script.py
@@ -1,17 +1,22 @@
 import numpy as np
 import pickle
 from PIL import Image
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 
 def load_data():
     with open('data/cifar-10-batches-py/data_batch_1', 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+        data_dict = pickle.load(fo, encoding='bytes')
+    with open('data/cifar-10-batches-py/batches.meta', 'rb') as fo:
+        labels_dict = pickle.load(fo, encoding='bytes')
+    return data_dict, labels_dict
 
 
-dict = load_data()
-data = dict[b'data']
-labels = np.array(dict[b'labels'])
+data_dict, labels_dict = load_data()
+data = data_dict[b'data']
+labels = np.array(data_dict[b'labels'])
+label_names = [s.decode('ascii') for s in labels_dict[b'label_names']]
 
 
 def show_img(data_item):
@@ -26,4 +31,20 @@ def mean(label):
     return np.mean(get_label_data(label), axis=0)
 
 
-show_img(mean(2))
+def mse(d1, d2):
+    return np.mean(np.sum((d1 - d2) ** 2, axis=1), axis=0)
+
+
+mses = []
+for category in range(10):
+    d = get_label_data(category)
+    pca = PCA(n_components=20)
+    recon = pca.fit_transform(d)  # sklearn.PCA centers automatically
+    recon = pca.inverse_transform(recon)
+    mses.append(mse(d, recon))
+
+x_pos = np.arange(len(mses))
+plt.bar(x_pos, mses, align='center')
+plt.xticks(x_pos, label_names)
+plt.ylabel('MSE')
+plt.show()
