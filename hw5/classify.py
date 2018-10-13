b4342c0426ce1f1c369e2dac215312dbf974a881
Mathias Jackermeier <mathias.jackermeier@tum.de>
2018-10-13 15:19:29 -0500

    Implemented class confusion matrix.

--- hw5/classify.py
+++ hw5/classify.py
@@ -1,14 +1,19 @@
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+np.random.seed(0)
 
 train_features = np.genfromtxt('computed_data/train_feature_vectors.csv', delimiter=',')
 train_labels = np.genfromtxt('computed_data/train_labels.csv', delimiter=',')
 test_features = np.genfromtxt('computed_data/test_feature_vectors.csv', delimiter=',')
 test_labels = np.genfromtxt('computed_data/test_labels.csv', delimiter=',')
-# p = np.random.permutation(len(features))
-# features = features[p]
-# labels = labels[p]
 
 rfc = RFC()
 rfc.fit(train_features, train_labels)
-print(rfc.score(test_features, test_labels))
+y_pred = rfc.predict(test_features)
+print(accuracy_score(test_labels, y_pred))
+c = confusion_matrix(test_labels, y_pred)
+class_errors = (np.sum(c, axis=1) - np.diagonal(c)) / np.sum(c, axis=1)
+c = np.hstack((c, class_errors.reshape(12, 1)))
