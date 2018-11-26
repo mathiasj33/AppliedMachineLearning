050e07ea9b0dac7abef1e301c468600994d06037
Mathias Jackermeier <mathias.jackermeier@tum.de>
2018-11-26 14:58:03 -0800

    Refactored code and added calculation of Q.

--- hw8/script.py
+++ hw8/script.py
@@ -4,23 +4,28 @@ np.random.seed(0)
 from sklearn.neighbors import NearestNeighbors as NN
 from scipy.stats import multivariate_normal as mvn
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
-img = Image.open('data/3.jpg')
+img_name = '2'
+img = Image.open('data/{}.jpg'.format(img_name))
 width, height = img.size
 img = np.array(img)
 img = img.reshape((width*height,3))
 img = np.asarray(img, np.float64)
 
-num_clusters = 50
-num_iter = 20
+num_clusters = 10
+max_iter = 10
+random_state = 0
 
 def init_params():
-    kmeans = KMeans(n_clusters=num_clusters, random_state=0, verbose=0).fit(img)
+    kmeans = KMeans(n_clusters=num_clusters, random_state=random_state, verbose=0, n_jobs=-1).fit(img)
     centers = kmeans.cluster_centers_
     pis = np.array([np.sum(kmeans.labels_ == i) / len(img) for i in range(num_clusters)])
     return centers, pis
 
 centers, pis = init_params()
+qs = []
+diffs = []
 
 def compute_new_weights():
     X = np.repeat(img[:,np.newaxis, :], num_clusters, axis=1)  # shape num_points x num_clusters x 3
@@ -43,10 +48,25 @@ def compute_new_centers_and_pis(weights):
     pis = np.sum(weights, axis=0) / len(img)
     return centers, pis
 
-for i in range(num_iter):
+def compute_q(weights):
+    X = np.repeat(img[:, np.newaxis, :], num_clusters, axis=1)  # shape num_points x num_clusters x 3
+    X = X - centers
+    X = np.sum(np.square(X), axis=2)
+    X *= -1/2
+    X += np.log(pis)
+    X *= weights
+    return np.sum(X)
+
+for i in range(max_iter):
     weights = compute_new_weights()
-    centers, pis = compute_new_centers_and_pis(weights)
-    print(i / num_iter)
+    new_centers, pis = compute_new_centers_and_pis(weights)
+    diffs.append(np.linalg.norm(new_centers - centers))
+    centers = new_centers
+    qs.append(compute_q(weights))
+    if diffs[-1] < 0.001:
+        print("Convergence after % iterations." % i)
+        break
+    print(i / max_iter)
 
 probs = np.zeros((len(img), num_clusters))
 for i in range(num_clusters):
@@ -57,4 +77,11 @@ for i in range(num_clusters):
 closest = np.argmax(probs, axis=1)  # obtain the cluster center with the highest posterior probability
 segmented = centers[closest]
 segmented = segmented.reshape((height, width, 3))
-Image.fromarray(np.asarray(segmented, np.uint8)).show()
+img = Image.fromarray(np.asarray(segmented, np.uint8))
+img.show()
+# img.save('/home/mathias/LaTeXProjects/Applied Machine Learning/hw8/res/{}_{}_r{}.png'.format(img_name, num_clusters, random_state))
+print(qs)
+plt.plot(qs)
+print(diffs)
+plt.figure()
+plt.plot(diffs)
